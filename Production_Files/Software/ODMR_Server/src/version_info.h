9350bb49905868168e50e1911c1e93f94aa08fec
Canan Gallitschke <canan.gallitschke@gmx.de>
2026-03-18 14:17:22 +0100

    Added Partner Logos

--- Production_Files/Software/ODMR_Server/src/version_info.h
+++ Production_Files/Software/ODMR_Server/src/version_info.h
@@ -5,10 +5,10 @@
 #define __VERSION_INFO_H__
 
 #define FIRMWARE_VERSION "1.0.0"
-#define BUILD_DATE "2026-03-13"
-#define BUILD_TIME "15:48:22"
-#define BUILD_TIMESTAMP "20260313154822"
-#define GIT_HASH "2e5e46b"
+#define BUILD_DATE "2026-03-18"
+#define BUILD_TIME "14:14:45"
+#define BUILD_TIMESTAMP "20260318141445"
+#define GIT_HASH "b971e35"
 #define GIT_BRANCH "fix-listoferrors"
 
 // Combined version string
