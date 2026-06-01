ce85a7ca575bab229495973b912cc664a956a645
Canan Gallitschke <canan.gallitschke@gmx.de>
2026-06-01 16:31:07 +0200

    Added Satuated Status to Justage Bar

--- Production_Files/Software/ODMR_Server/src/version_info.h
+++ Production_Files/Software/ODMR_Server/src/version_info.h
@@ -6,10 +6,10 @@
 
 #define FIRMWARE_VERSION "1.0.0"
 #define BUILD_DATE "2026-06-01"
-#define BUILD_TIME "15:59:43"
-#define BUILD_TIMESTAMP "20260601155943"
-#define GIT_HASH "2dae748"
-#define GIT_BRANCH "main"
+#define BUILD_TIME "16:18:55"
+#define BUILD_TIMESTAMP "20260601161855"
+#define GIT_HASH "6c11092"
+#define GIT_BRANCH "Adjust-Justage-Values"
 
 // Combined version string
 #define VERSION_STRING "v" FIRMWARE_VERSION " (" BUILD_DATE " " BUILD_TIME " - " GIT_HASH ")"
