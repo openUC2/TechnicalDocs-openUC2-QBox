6c11092f1f76cc4c6c0a84b5ca9b22f4e231b2aa
Canan Gallitschke <canan.gallitschke@gmx.de>
2026-06-01 16:03:51 +0200

    Added a lookup table for maximal  Intensity Values, depending on gain and integration time & Adjust Optimal Values

--- Production_Files/Software/ODMR_Server/src/version_info.h
+++ Production_Files/Software/ODMR_Server/src/version_info.h
@@ -5,11 +5,11 @@
 #define __VERSION_INFO_H__
 
 #define FIRMWARE_VERSION "1.0.0"
-#define BUILD_DATE "2026-05-21"
-#define BUILD_TIME "20:56:51"
-#define BUILD_TIMESTAMP "20260521205651"
-#define GIT_HASH "f1d6d8b"
-#define GIT_BRANCH "fix-listoferrors"
+#define BUILD_DATE "2026-06-01"
+#define BUILD_TIME "15:59:43"
+#define BUILD_TIMESTAMP "20260601155943"
+#define GIT_HASH "2dae748"
+#define GIT_BRANCH "main"
 
 // Combined version string
 #define VERSION_STRING "v" FIRMWARE_VERSION " (" BUILD_DATE " " BUILD_TIME " - " GIT_HASH ")"
